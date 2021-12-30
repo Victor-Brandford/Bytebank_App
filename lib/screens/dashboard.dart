3f83f6cb8061db446a5aabf755056d80421d7006
Victor Hugo <72666385+Victor-Brandford@users.noreply.github.com>
2021-12-30 16:57:20 -0400

    adição de um web API

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,9 +1,8 @@
 import 'package:bytebank_salvamento_arquivos/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
+import 'Contacts.dart';
 
 class Dashboard extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,27 +21,46 @@ class Dashboard extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             child: Row(
               children: [
-
-                _FeatureItem('Transfer', Icons.monetization_on,onClick:(){
-                  _showContactslist(context);
-                },),
-                _FeatureItem('Transaction Feed', Icons.description, onClick: (){print('transfer was clicked');}),
-                _FeatureItem('Transaction Feed', Icons.description, onClick: (){print('transfer was clicked');}),
+                _FeatureItem(
+                  'Transfer',
+                  Icons.monetization_on,
+                  onClick: () {
+                    _showContactslist(context);
+                  },
+                ),
+                _FeatureItem('Transaction Feed', Icons.description,
+                    onClick: () {
+                  _showTransactionFeed(context);
+                }),
+                _FeatureItem('Transaction Feed', Icons.description,
+                    onClick: () {
+                  print('transfer was clicked');
+                }),
               ],
             ),
           ),
         ],
       ),
-
     );
   }
-  void _showContactslist(BuildContext context){
+
+  void _showTransactionFeed(BuildContext context) {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => ContactsList(),
+      MaterialPageRoute(
+        builder: (context) => TransactionsList(),
+      ),
+    );
+  }
+
+  void _showContactslist(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ContactsList(),
       ),
     );
   }
 }
+
 class _FeatureItem extends StatelessWidget {
   final String name;
   final IconData icon;
@@ -52,19 +70,18 @@ class _FeatureItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Material(
         color: Theme.of(context).primaryColor,
         child: InkWell(
-          onTap: (){
+          onTap: () {
             onClick!();
           },
           child: Container(
             padding: EdgeInsets.all(8.0),
             width: 150,
             height: 100,
-
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
