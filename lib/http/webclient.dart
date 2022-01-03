e3c6a6a7d2720a25b1c69da487273e2a4993025f
Victor Hugo <72666385+Victor-Brandford@users.noreply.github.com>
2022-01-03 17:54:08 -0400

    adição do client de resquest and response

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,31 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
-void findAll() async{
-  final Response response = await get(Uri.parse('http://192.168.1.109:8080/transactions?'));
-  print(response.body);
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('Status Code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
+
+void findAll() async {
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response =
+      await client.get(Uri.http('192.168.1.109:8080', 'transactions'));
+
 }
